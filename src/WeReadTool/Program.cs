d3cb479903b30c82b73f338c65b5b2c05230d677
Ray <zai7lou@outlook.com>
2023-04-23 01:44:08 +0800

    update

--- src/WeReadTool/Program.cs
+++ src/WeReadTool/Program.cs
@@ -23,7 +23,11 @@ public class Program
 
     public static async Task<int> Main(string[] args)
     {
-        Microsoft.Playwright.Program.Main(new string[] { "install", "--with-deps", "chromium" });
+        var exitCode = Microsoft.Playwright.Program.Main(new string[] { "install", "--with-deps chromium" });
+        if (exitCode != 0)
+        {
+            throw new Exception($"Playwright exited with code {exitCode}");
+        }
 
         Log.Logger = CreateLogger(args);
         try
