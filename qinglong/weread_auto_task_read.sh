fb82ee91fdccdfab277eabfbf32270eb247564ba
Ray <zai7lou@outlook.com>
2023-05-02 11:24:34 +0800

    update

--- qinglong/weread_auto_task_read.sh
+++ qinglong/weread_auto_task_read.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # new Env("weread每日阅读任务")
-# cron 0 7,20,21 * * * weread_auto_task_read.sh
+# cron 0 7,8,15,16,20,21 * * * weread_auto_task_read.sh
 . weread_auto_task_base.sh
 
 cd ./src/WeReadTool
